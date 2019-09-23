21d67aad205b629b29776f13793bf8273a7146ee
alcasinoroyale <alexzdatny@gmail.com>
2019-09-23 15:02:26 -0400

    Add route methods to controller, insert link from post show page, and create comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,31 @@ class CommentsController < ApplicationController
   def index
     @comments = Comment.all
   end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = @post.id
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id, :user_id)
+  end
 end
